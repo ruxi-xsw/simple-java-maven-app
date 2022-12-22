6a5c466554efae96d9e2dffa6f940ee23ca2e202
xushuaiwei.xsw <xushuaiwei.xsw@fliggy.com>
2022-12-22 11:44:57 +0800

    新建分支

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,7 +5,7 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "Hello World!";
+    private static final String MESSAGE = "这是Test分支";
 
     public App() {}
 
